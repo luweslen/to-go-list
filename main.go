1a0e770c32665476cdb427941ec4f7f5e4e07268
Alyson Vilela <alyson.nascimento@zrp.com.br>
2025-04-23 16:08:42 -0300

    feat: add db

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/luweslen/to-go-list/internal/app"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Todo = app.Todo
 
+var DB *sql.DB
+
+func loadEnv() {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func initDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "./app.db") // Open a connection to the SQLite database file named app.db
+	if err != nil {
+		log.Fatal(err) // Log an error and stop the program if the database can't be opened
+	}
+
+	// SQL statement to create the todos table if it doesn't exist
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS todos (
+	 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	 title TEXT,
+	 description TEXT,
+	 done BOOL
+	);`
+
+	_, err = DB.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("Error creating table: %q: %s\n", err, sqlStmt) // Log an error if table creation fails
+	}
+}
+
 func main() {
+	loadEnv()
+	initDB()
+
 	todos := []Todo{}
 
 	todo := Todo{
@@ -23,7 +60,7 @@ func main() {
 	oldTodo.Toggle().SetDescription("Estudei Go").SetName("Estudando Go")
 	app.FindById(todos, 1)
 
-	fmt.Println(&todo.Id)
-	fmt.Println(&todos[0].Id)
-	fmt.Println(&oldTodo.Id)
+	log.Print(&todo.Id)
+	log.Print(&todos[0].Id)
+	log.Print(&oldTodo.Id)
 }
